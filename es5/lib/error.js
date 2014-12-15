ac9874b152b547dddbd955f8f11e5b3c8b2637d7
Soares Chen <soares.chen@gmail.com>
2014-12-16 02:19:47 +0800

    Revamped quiver-error down to bare minimum.

---
+++ es5/lib/error.js
@@ -0,0 +1,44 @@
+"use strict";
+Object.defineProperties(exports, {
+  error: {get: function() {
+      return error;
+    }},
+  __esModule: {value: true}
+});
+var error = (function(errorCode, errorMessage, details) {
+  return new ServerError(errorCode, errorMessage, details);
+});
+var ServerError = function ServerError() {
+  var errorCode = arguments[0] !== (void 0) ? arguments[0] : 500;
+  var errorMessage = arguments[1] !== (void 0) ? arguments[1] : '';
+  var details = arguments[2] !== (void 0) ? arguments[2] : {};
+  $traceurRuntime.superConstructor($ServerError).call(this, errorMessage);
+  Error.captureStackTrace(this, error);
+  this._errorCode = errorCode;
+  this._errorMessage = errorMessage;
+  this._errorDetails = details;
+};
+var $ServerError = ServerError;
+($traceurRuntime.createClass)(ServerError, {
+  get name() {
+    return 'ServerError';
+  },
+  get code() {
+    return this._errorCode;
+  },
+  get errorCode() {
+    return this._errorCode;
+  },
+  get message() {
+    return this._errorMessage;
+  },
+  get details() {
+    return this._errorDetails;
+  },
+  format: function() {
+    return ("[Error " + this.errorCode + ": " + this.message + "]\n" + this.stack);
+  },
+  inspect: function() {
+    return this.format();
+  }
+}, {}, Error);
--- test/test.js
+++ test/test.js
@@ -1,56 +1,15 @@
+import 'traceur'
+import { error } from '../lib/error'
 
-var error = require('../lib/error')
-var should = require('should')
+import chai from 'chai'
+var should = chai.should()
 
-var noAsyncError = function(callback) {
-  process.nextTick(function() {
-    callback(error.error(500, 'test error'))
-  })
-}
-
-var withAsyncError = function(callback) {
-  var asyncErr = error.asyncError()
-  process.nextTick(function() {
-    callback(asyncErr(error.error(500, 'test error')))
-  })
-}
-
-var nestedAsyncError = function(callback) {
-  var asyncErr = error.asyncError()
-  noAsyncError(function(err) {
-    callback(asyncErr(err))
-  })
-}
-
-describe('test async error', function() {
-  it('normal error only has async stack trace', function() {
-    noAsyncError(function(err) {
-      should.not.exist(err.syncStack)
-      console.log('error without sync stack:')
-      console.log(err.stack)
-    })
-  })
-
-  it('async error shoud have both stack traces', 
-  function(callback) {
-    withAsyncError(function(err) {
-      var callstack = err.callstacks[0]
-      should.exist(callstack.sync)
-      should.exist(callstack.async)
-      console.log('error with sync stack:')
-      console.log(err)
-
-      callback()
-    })
-  })
-
-  it('async error add sync stack when propogating', function() {
-    nestedAsyncError(function(err) {
-      var callstack = err.callstacks[0]
-      should.exist(callstack.sync)
-      should.exist(callstack.async)
-      console.log('error with async stack:')
-      console.log(err)
-    })
+describe('quiver error test', () => {
+  it('basic error test', () => {
+    var err = error(404, 'Not Found')
+    err.code.should.equal(404)
+    err.errorCode.should.equal(404)
+    err.message.should.equal('Not Found')
+    console.log(err)
   })
 })
