e011337ab1cea32e4b8b53e8880b9d00232e9ba3
Soares Chen <soares.chen@gmail.com>
2015-03-11 01:08:32 +0800

    Switch to Babel.js ES6 transpiler.

--- src/test/test.js
+++ src/test/test.js
@@ -1,12 +1,11 @@
-import 'traceur'
 import { error } from '../lib/error'
 
 import chai from 'chai'
-let should = chai.should()
+const should = chai.should()
 
 describe('quiver error test', () => {
   it('basic error test', () => {
-    let err = error(404, 'Not Found')
+    const err = error(404, 'Not Found')
     err.code.should.equal(404)
     err.errorCode.should.equal(404)
     err.message.should.equal('Not Found')
